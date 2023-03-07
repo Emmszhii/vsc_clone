eb9287a527a436cd6cc884e15143108fa1261654
unknown <emmanuelmanalo707@gmail.com>
2023-03-07 11:33:04 +0800

    about home

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const onClickBurgerBtn = (e) => {
 };
 
 burgerBtn.addEventListener("click", onClickBurgerBtn);
+
+let slider = tns({
+  container: ".socials__wrapper",
+  mouseDrag: true,
+  items: 1,
+  gutter: 20,
+  controlsText: [
+    '<i class="fa-solid fa-chevron-left"></i>',
+    '<i class="fa-solid fa-chevron-right"></i>',
+  ],
+  slideBy: "page",
+  autoPlay: false,
+  axis: "horizontal",
+  responsive: {
+    1020: {
+      items: 3,
+    },
+  },
+});
